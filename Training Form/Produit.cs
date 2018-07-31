8f1241a0e7b0300befd67669bf1713bdbef097dd
audrey arena <forbidden077@gmail.com>
2018-07-31 14:41:19 +0200

    definition attributs Produit

--- Training Form/Produit.cs
+++ Training Form/Produit.cs
@@ -8,7 +8,33 @@ namespace Training_Form
 {
     class Produit
     {
+        private int _codeProduit;
+        private string _nom;
+        private string _description;
 
-
+        public int CodeProduit
+        {
+            get { return _codeProduit; }
+            set
+            {
+                _codeProduit = value;
+            }
+        }
+        public string Nom
+        {
+            get { return _nom; } 
+            set
+            {
+                _nom = value;
+            }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+            }
+        }
     }
 }
