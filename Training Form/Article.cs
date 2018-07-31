5acaccbb2ebb91404044e58aa8c854f2b67d88ca
audrey arena <forbidden077@gmail.com>
2018-07-31 14:59:18 +0200

    Classe Article

---
+++ Training Form/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Training_Form
+{
+    class Article : Produit
+    {
+        private Image _imageProduit;
+
+        public Image ImageProduit
+        {
+            get { return _imageProduit; }
+            set
+            {
+                _imageProduit = value;
+            }
+        }
+    }
+}
