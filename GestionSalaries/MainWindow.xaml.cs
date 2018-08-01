2e766a080b21624357405c5c3e52d56948f37d93
Cdi <Cdi@CDI07>
2018-08-01 11:40:56 +0200

    liste des salarie et formulaire d'ajout

---
+++ GestionSalaries/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestionSalaries
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MalisteSalaries maListeSalaries;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            maListeSalaries = new MalisteSalaries();
+            TableauSalaries.ItemsSource = maListeSalaries;
+
+            maListeSalaries.Add(new Salarie("aa123aa", "toto", "martin", "coach", new DateTime(2000, 10, 23), "azerty", "mail@domaine.fr"));
+            maListeSalaries.Add(new Salarie("aa123aa", "flo", "carrey", "coach", new DateTime(2000, 10, 23), "azerty", "mail@domaine.fr"));
+            maListeSalaries.Add(new Salarie("aa123aa", "julien", "moreau", "coach", new DateTime(2000, 10, 23), "azerty", "mail@domaine.fr"));
+            maListeSalaries.Add(new Salarie("aa123aa", "anthony", "arena", "gerant", new DateTime(2000, 10, 23), "azerty", "mail@domaine.fr"));
+            
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BoutonAjouterSalarie_Click(object sender, RoutedEventArgs e)
+        {
+            FormSalarie formSalarie = new FormSalarie();
+            formSalarie.ShowDialog();
+
+        }
+    }
+
+
+}
---
+++ GestionSalaries/Salarie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionSalaries
+{
+    public class Salarie
+    {
+        private string _numClient;
+        private string _nom;
+        private string _prenom;
+        private string _role;
+        private DateTime _dateNaissance;
+        private string _password;
+        private string _mail;
+
+        
+       //rivate List<Salarie> MalisteSalaries { get ; set };
+
+
+        public Salarie()
+        {
+        }
+
+        public Salarie (string numClient, string nom, string prenom, string role, DateTime dateNaissance, string password, string mail)
+            :this()
+        {
+            NumClient = numClient;
+            Nom = nom;
+            Prenom = prenom;
+            Role = role;
+            DateNaissance = dateNaissance;
+            Password = password;
+            Mail = mail;
+        }
+
+
+        public string NumClient { get => _numClient; set => _numClient = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+        public string Role { get => _role; set => _role = value; }
+        public DateTime DateNaissance { get => _dateNaissance; set => _dateNaissance = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Mail { get => _mail; set => _mail = value; }
+
+
+        
+
+        
+    }
+    public class MalisteSalaries : List<Salarie> { }
+
+}
+
