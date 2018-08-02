404f283770a5fa33bb6e419d1baaf160a730bcda
alan audivert <alan.audivert@hotmail.fr>
2018-08-02 11:49:05 +0200

    nettoyage Salarie.cs

--- Training Form/Salarie.cs
+++ Training Form/Salarie.cs
@@ -8,48 +8,55 @@ namespace Training_Form
 {
     public class Salarie : User
     {
-        private string _numSalarie;
-        private string _role;
+        #region variables
         private DateTime _dateEmbauche;
         private string _password;
-
-        public Salarie(string nom, string prenom, string mail, DateTime dateNaissance, Permissions permission, string numSalarie, string role, DateTime dateEmbauche, string password)
-           : base( nom, prenom, mail, dateEmbauche, permission)
-        {
-            NumSalarie = numSalarie;
-            Role = role;
-            DateEmbauche = dateEmbauche;
-            Password = password;
-        }
-
-        public string NumSalarie
-        {
-            get { return _numSalarie; }
-            set { _numSalarie = value; }
-        }
-
-        public string Role
-        {
-            get { return _role; }
-            set { _role = value; }
-        }
 
         public DateTime DateEmbauche
         {
             get { return _dateEmbauche; }
-            set { _dateEmbauche = value; }
+            set
+            {
+                DateTime stock = _dateEmbauche;
+                BetterNotifyPropertyChanging(stock, value);
+                if (argsChanging == null || !argsChanging.Cancel)
+                {
+                    _dateEmbauche = value;
+                    BetterNotifyPropertyChanged(stock, value);
+                }
+            }
         }
 
         public string Password
         {
             get { return _password; }
-            set { _password = value; }
+            set
+            {
+                string stock = _password;
+                BetterNotifyPropertyChanging(stock, value);
+                if (argsChanging == null || !argsChanging.Cancel)
+                {
+                    _password = value;
+                    BetterNotifyPropertyChanged(stock, value);
+                }
+            }
         }
-        
+        #endregion
+
+        #region constructeurs
+        public Salarie(string nom, string prenom, string mail, DateTime dateNaissance, Permissions permission, DateTime dateEmbauche, string password)
+           : base(nom, prenom, mail, dateEmbauche, permission)
+        {
+            DateEmbauche = dateEmbauche;
+            Password = password;
+        }
+        #endregion
+
+        #region methodes
         public override bool Equals(object obj)
         {
             Salarie sal = obj as Salarie;
-            if (sal==null)
+            if (sal == null)
             {
                 return false;
             }
@@ -61,12 +68,14 @@ namespace Training_Form
 
         public bool Equals(Salarie sal)
         {
-            return this.NumSalarie == sal.NumSalarie;
+            return base.Identifiant == sal.Identifiant;
         }
+
         public override int GetHashCode()
         {
-            return NumSalarie.GetHashCode();
+            return Identifiant.GetHashCode();
         }
+        #endregion
     }
 }
 
