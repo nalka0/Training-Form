999b7a91538a102fee9614ba280431366fffb077
LapezeMathieu <lapezemathieu@hotmail.fr>
2018-08-22 15:42:02 +0200

    Modification Bouton "ajoutElement" Dashboard

--- Training Form/MainWindow.xaml.cs
+++ Training Form/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -7,8 +8,26 @@ namespace Training_Form
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    /// 
+
+    public partial class MainWindow : Window { 
+
+        private int _panelActif;
+
+
+        public int PanelActif
+        {
+            get
+            {
+                return _panelActif;
+            }
+            set
+            {
+                _panelActif = value;
+
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,34 +55,49 @@ namespace Training_Form
 
         private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int index = ListViewMenu.SelectedIndex;
+            
             GridMain.Children.Clear();
-
+            int index = ListViewMenu.SelectedIndex;
+            PanelActif = index;
             switch (index)
             {
                 case 0:
                     GridMain.Children.Clear();
                     GridMain.Children.Add(new UserControlHome());
+                    recherche.Visibility = Visibility.Collapsed;
+                    iconElement.Kind = MaterialDesignThemes.Wpf.PackIconKind.Face;
                     break;
                 case 1:
                     GridMain.Children.Clear();
                     GridMain.Children.Add(new UserControlClients());
+                    recherche.Visibility = Visibility.Visible;
+                    iconElement.Kind = MaterialDesignThemes.Wpf.PackIconKind.Face;
                     break;
                 case 2:
                     GridMain.Children.Clear();
                     GridMain.Children.Add(new UserControlProduits());
+                    recherche.Visibility = Visibility.Visible;
+                    iconElement.Kind = MaterialDesignThemes.Wpf.PackIconKind.CartPlus;
                     break;
                 case 3:
                     GridMain.Children.Clear();
                     GridMain.Children.Add(new UserControlServices());
+                    recherche.Visibility = Visibility.Visible;
+                    iconElement.Kind = MaterialDesignThemes.Wpf.PackIconKind.Basketball;
                     break;
                 case 4:
                     GridMain.Children.Clear();
                     GridMain.Children.Add(new UserControlSalarie());
+                    recherche.Visibility = Visibility.Visible;
+                    iconElement.Kind = MaterialDesignThemes.Wpf.PackIconKind.Worker;
                     break;
                 default:
                     break;
             }
+        }
+        private void ajoutElement_Click(object sender, RoutedEventArgs e)
+        {
+            PanelActif = PanelActif;
         }
     }
 }
